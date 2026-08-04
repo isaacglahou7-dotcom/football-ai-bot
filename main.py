e4aac1e6a4bd2c9402ae731a0eb0179ebbad5d44
isaacglahou7-dotcom <isaacglahou7@gmail.com>
2026-08-04 23:42:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+import threading
+import requests
+import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+FOOTBALL_API_KEY = os.getenv("FOOTBALL_API_KEY")
+
+BASE_URL = "https://v3.football.api-sports.io"
+
+HEADERS = {
+    "x-apisports-key": FOOTBALL_API_KEY
+}
+
+CACHE = {}
+CACHE_TIME = 300  # 5 minutes
+def api_get(endpoint, params=None):
+    key = endpoint + str(params)
+
+    if key in CACHE:
+        if time.time() - CACHE[key]["time"] < CACHE_TIME:
+            return CACHE[key]["data"]
+
+    response = requests.get(
+        BASE_URL + endpoint,
+        headers=HEADERS,
+        params=params,
+        timeout=20
+    )
+
+    data = response.json()
+
+    CACHE[key] = {
+        "time": time.time(),
+        "data": data
+    }
+
+    return data
